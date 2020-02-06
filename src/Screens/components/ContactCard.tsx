2e29fda3ee7a0b82de8b0d2ddd9e6cb7c29cd3a0
riyazpasha <riyazpasha.mohammed@thoughtworks.com>
2020-02-06 18:57:22 +0530

    [Riyaz] Add. Will show snack on contact button click

--- src/Screens/components/ContactCard.tsx
+++ src/Screens/components/ContactCard.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import { yellowSnack } from '../../Services/SnackService'
 
 const ContactCard = ({ contact }) => {
     return (
-        <View style={styles.container}>
+        <TouchableOpacity style={styles.container} onPress={()=>{yellowSnack("clicked")}}>
             <Text>{contact.givenName}</Text>
-        </View>
+        </TouchableOpacity>
     )
 }
 
